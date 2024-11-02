03946ecf0cc18f2df090a992e9d3af6c3025c46e
hamalala <46687725+hamalala@users.noreply.github.com>
2024-11-02 17:14:01 +0700

    Create SED690-project2-group-704-710-712-714.py

---
+++ SED690-project2-group-704-710-712-714.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+# Set title
+st.title("Discovering Frequent Patterns and Association Rules")
+st.write("Group: 704-710-712-714")
+st.markdown('[Can edit this sheet for your transaction](https://docs.google.com/spreadsheets/d/1-h4q2swBlPGO76pJkExdtIoVoMyoePjkovyk71c-PzA)')
